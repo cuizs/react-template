0ef712a6a94abbe8705eafb85b41e6944615b941
Ryan <cuizhanshan1025@163.com>
2025-12-10 18:01:41 +0800

    test: vitest config

--- types/Foo.ts
+++
@@ -1,9 +0,0 @@
-// export type FooParams = {
-//   items?: Array<{ quantity: number }>;
-// };
-
-type FooParamsItems = { quantity: number };
-
-export interface FooParams {
-  items?: Array<FooParamsItems>;
-}
